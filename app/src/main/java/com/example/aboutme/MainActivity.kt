f87886ef81fb87ca68367e69667ac24d5d519af5
ndhu_csie <sa51993forest@gmail.com>
2021-03-22 17:16:25 +0800

    3/22 aboutme 2

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aboutme
 
+import android.app.Activity
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,41 +9,48 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var myName: MyName = MyName("Timmy","boy")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.done_button).setOnClickListener() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //binding效果等同findViewById但不會花那麼多的ＣＯＳＴ
+        binding.doneButton.setOnClickListener() {
             addNickname(it) //it代表button
         }
 
-        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+        binding.nicknameText.setOnClickListener {
             updateNickname(it)
         }
+
+        binding.myName = myName
     }
 
     private fun addNickname(view: View){
-        val editText: EditText = findViewById(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
         //上面兩種findViewById結果皆相同
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE //因為使用button參數呼叫fun view直接代表button
-        nicknameTextView.visibility = View.VISIBLE
-
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            //invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE //因為使用button參數呼叫fun view直接代表button
+            nicknameText.visibility = View.VISIBLE
+        }
         // 隱藏鍵盤
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun updateNickname(view: View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val doneButton = findViewById<Button>(R.id.done_button)
+        val editText = binding.nicknameEdit
+        val doneButton = binding.doneButton
 
-        editText.visibility = View.VISIBLE
-        doneButton.visibility = View.VISIBLE
+        binding.nicknameEdit.visibility = View.VISIBLE
+        binding.doneButton.visibility = View.VISIBLE
         view.visibility = View.GONE
 
         // Set the focus to the edit text.
